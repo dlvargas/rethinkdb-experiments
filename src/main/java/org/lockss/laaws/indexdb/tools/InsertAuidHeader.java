83e7a97b55b4b00ea269f5766d0c1c96929f2ba6
dlvargas <dlvargas@stanford.edu>
2016-07-13 00:01:56 -0700

    Start of InsertAuidHeader tool

--- src/main/java/org/lockss/laaws/indexdb/tools/InsertAuidHeader.java
+++ src/main/java/org/lockss/laaws/indexdb/tools/InsertAuidHeader.java
@@ -1,4 +1,53 @@
+/*
+
+Copyright (c) 2016, Board of Trustees of Leland Stanford Jr. University,
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
 package org.lockss.laaws.indexdb.tools;
 
+import org.jwat.warc.WarcReaderFactory;
+
 public class InsertAuidHeader {
+    // For expedience we use static variables for now
+    private static String warc_in  = "/home/daniel/ia-test.warc";
+    private static String warc_out = "/home/daniel/ia-test.new.warc";
+    private static String auid     = "org.lockss.test:gibberish";
+
+    public InsertAuidHeader(String in, String out, String auid) {
+        WarcReaderFactory.getReader(z)
+    }
+
+    public static void main(String[] args) {
+        InsertAuidHeader injector = new InsertAuidHeader(warc_in, warc_out, auid);
+
+        //WarcWriterFactory.getWriterUncompressed();
+    }
+
 }
